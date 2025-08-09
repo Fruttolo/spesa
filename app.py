54d8af27edf810bb4ac8f3cddea9b0f0a2f29e8b
fruttolo <s.contino97@gmail.com>
2025-08-10 01:30:51 +0200

    update app.run to listen on all interfaces

--- app.py
+++ app.py
@@ -393,4 +393,4 @@ def genera_lista_spesa():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=28000)
+    app.run(debug=True, host='0.0.0.0', port=28000)
